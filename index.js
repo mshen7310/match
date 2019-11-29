0a200ee2222f34194407ab5499662953d8ef1f36
feng.shen <mshen@sparkpad.com>
2019-11-29 19:48:39 +0800

    add additional argument

--- index.js
+++ index.js
@@ -31,24 +31,24 @@ function is_regexp(x){
 	return x instanceof RegExp;
 }
 function optional(pattern){
-	return function (x){
-		return (typeof x == 'undefined') || match(x, pattern);
+	return function (x, acc){
+		return (typeof x == 'undefined') || match(x, pattern, acc);
 	};
 }
 function and(...patterns){
-	return function(x){
-		return patterns.reduce((a,c)=>a && match(x, c), true);
+	return function(x, acc){
+		return patterns.reduce((a,c)=>a && match(x, c, acc), true);
 	};
 }
 function or(...patterns){
-	return function(x){
-		return patterns.reduce((a,c)=>a || match(x, c), false);
+	return function(x, acc){
+		return patterns.reduce((a,c)=>a || match(x, c, acc), false);
 	};
 }
 function not(...patterns){
 	let or_func = or(...patterns);
-	return function(x){
-		return !or_func(x);
+	return function(x, acc){
+		return !or_func(x, acc);
 	};
 }
 
@@ -69,27 +69,27 @@ function type_of(x){
 }
 const dispatch_table = {
 	primitive:{
-		primitive: function(x, ptn){
-			return x == ptn;
+		primitive: function(x, ptn, acc){
+			return acc(x == ptn, x);
 		},
-		array: function(x, ptn){
-			return false;
+		array: function(x, ptn, acc){
+			return acc(false, x);
 		},
-		regexp: function(x, ptn){
-			return ptn.test(x.toString());
+		regexp: function(x, ptn, acc){
+			return acc(ptn.test(x.toString()), x);
 		},
-		object: function(x, ptn){
-			return false;
+		object: function(x, ptn, acc){
+			return acc(false, x);
 		}
 	},
 	array:{
-		primitive: function(x, ptn){
-			return false;
+		primitive: function(x, ptn, acc){
+			return acc(false, x);
 		},
-		array: function(x, ptn){
+		array: function(x, ptn, acc){
 			function match_array(u){
 				for(var j=0; j < ptn.length; ++j){
-					if(match(u, ptn[j]))
+					if(match(u, ptn[j], acc))
 						return true;
 				}
 				return false;
@@ -97,91 +97,93 @@ const dispatch_table = {
 			if(ptn.length > 0){
 				for(var i=0; i < x.length; ++i){
 					if(!match_array(x[i])){
-						return false;
+						return acc(false, x);
 					}
 				}	
 			}
-			return true;
+			return acc(true, x);
 		},
-		regexp: function(x, ptn){
-			return ptn.test(x.toString());
+		regexp: function(x, ptn, acc){
+			return acc(ptn.test(x.toString()), x);
 		},
-		object: function(x, ptn){
-			return false;
+		object: function(x, ptn, acc){
+			return acc(false, x);
 		}
 	},
 	regexp:{
-		primitive: function(x, ptn){
-			return x.toString() == ptn;
+		primitive: function(x, ptn, acc){
+			return acc(x.toString() == ptn, x);
 		},
-		array: function(x, ptn){
-			return false;
+		array: function(x, ptn, acc){
+			return acc(false, x);
 		},
-		regexp: function(x, ptn){
-			return x.toString() == ptn.toString();
+		regexp: function(x, ptn, acc){
+			return acc(x.toString() == ptn.toString(), x);
 		},
-		object: function(x, ptn){
-			return false;
+		object: function(x, ptn, acc){
+			return acc(false, x);
 		}
 	},
 	object:{
-		primitive: function(x, ptn){
-			return false;
+		primitive: function(x, ptn, acc){
+			return acc(false, x);
 		},
-		array: function(x, ptn){
-			return false;
+		array: function(x, ptn, acc){
+			return acc(false, x);
 		},
-		regexp: function(x, ptn){
-			return ptn.test(x.toString());
+		regexp: function(x, ptn, acc){
+			return acc(ptn.test(x.toString()), x);
 		},
-		object: function(x, ptn){
+		object: function(x, ptn, acc){
 			if(x && ptn){
 				const ks = Object.keys(ptn);
 				for(var i = 0; i< ks.length; ++i){
-					if(!match(x[ks[i]], ptn[ks[i]])){
+					if(!match(x[ks[i]], ptn[ks[i]], acc)){
 						// console.log(ks[i],'mismatch');
 						// console.log(x[ks[i]], '*****', ptn[ks[i]]);
-						return false;
+						return acc(false, x[ks[i]]);
 					}
 				}
-				return true;    
+				return acc(true, x);    
 			}
-			return !(x || ptn);
+			return acc(!(x || ptn), x);
 		}
 	}    
 };
 
-function match(x, ptn){
+function match(x, ptn, acc){
+	acc = is_function(acc) ? acc : x => x;
 	const x_type = type_of(x);
 	const ptn_type = type_of(ptn);
 	if(x_type == 'unknown' || ptn_type == 'unknown'){
-		return false;
+		return acc(false, x);
 	}
 	if(ptn_type == 'function'){
-		return ptn(x);
+		return acc(ptn(x, acc), x);
 	}
 	if(x_type == 'function'){
-		return false;
+		return acc(false, x);
 	}
-	return dispatch_table[x_type][ptn_type](x, ptn);
+	return dispatch_table[x_type][ptn_type](x, ptn, acc);
 }
-match.is_primitive = is_primitive;
-match.is_array = is_array;
-match.is_function = is_function;
-match.is_object = is_object;
-match.is_regexp = is_regexp;
-match.is_number = is_number;
-match.is_boolean = is_boolean;
-match.is_undefined = is_undefined;
-match.is_null = is_null;
-match.is_string = is_string;
-match.optional = optional;
-match.and = and;
-match.all = and;
-match.or = or;
-match.any = or;
-match.none = not;
-match.not = not;
-match.is_empty = x => (is_array(x) && x.length == 0) || (is_object(x) && Object.keys(x).length == 0) || (is_string(x) && x.length == 0);
-module.exports = match;
+
+match.primitive 	= match.is_primitive 	= is_primitive;
+match.array 		= match.is_array 		= is_array;
+match.function 		= match.is_function 	= is_function;
+match.object 		= match.is_object 		= is_object;
+match.regexp 		= match.is_regexp	 	= is_regexp;
+match.number 		= match.is_number 		= is_number;
+match.boolean 		= match.is_boolean 		= is_boolean;
+match.undefined 	= match.is_undefined 	= is_undefined;
+match.null 			= match.is_null 		= is_null;
+match.string		= match.is_string 		= is_string;
+match.optional 		= optional;
+match.and 			= and;
+match.all 			= and;
+match.or 			= or;
+match.any 			= or;
+match.none 			= not;
+match.not 			= not;
+match.empty			= match.is_empty 		= x => (is_array(x) && x.length == 0) || (is_object(x) && Object.keys(x).length == 0) || (is_string(x) && x.length == 0);
+module.exports		= match;
 
